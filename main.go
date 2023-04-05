9986e6cc22e532e6a78ae2e8d4d92ed6f1ceadfa
Greta Masiulyte <gretos.masiulytes@gmail.com>
2023-04-05 22:14:31 +0300

    add Kixart web scrapper

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gocolly/colly"
+	"log"
+	"net/http"
+	"os"
+)
+
+type item struct {
+	Name   string `json:"name"`
+	Price  string `json:"price"`
+	ImgUrl string `json:"imgUrl"`
+}
+
+func main() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/nike", kixartNike)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func kixartNike(w http.ResponseWriter, r *http.Request) {
+	c := colly.NewCollector()
+
+	var items []item
+
+	c.OnHTML("a[class=pr_productItem]", func(element *colly.HTMLElement) {
+		item := item{
+			Name:   element.ChildText("div.bottom div.title"),
+			Price:  element.ChildText("div.bottom div.price"),
+			ImgUrl: element.ChildAttr("div.productTop img.mainPic", "src"),
+		}
+		items = append(items, item)
+	})
+
+	c.OnHTML("a.next", func(element *colly.HTMLElement) {
+		nextPage := element.Request.AbsoluteURL(element.Attr("href"))
+		c.Visit(nextPage)
+	})
+
+	c.OnRequest(func(request *colly.Request) {
+		fmt.Println(request.URL.String())
+	})
+
+	c.Visit("https://www.kixart.lt/kedai-laisvalaikiui?vars/manufacturer/1")
+
+	content, err := json.MarshalIndent(items, "", "    ")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	err = os.WriteFile("KixartNikeProducts.json", content, 0644)
+	if err != nil {
+		fmt.Fprintf(w, "Error writing file: ")
+	}
+	json.NewEncoder(w).Encode(content[0])
+}
